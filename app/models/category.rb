ca61d763ad76a3e273f50ac8cf7d32f7db2e9c6d
Setiadi <setiadi.seth@gmail.com>
2023-07-11 14:21:04 +0700

    Render Images on Index page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
   has_many :plays
-  has_attached_file :play_img, styles: { play_index: "150x150>", play_show: "512x512>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
+  
 end
