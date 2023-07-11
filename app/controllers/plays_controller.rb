3bdb1bfd16e869414ba7bff1d0d8f6167dc9e3b9
Setiadi <setiadi.seth@gmail.com>
2023-07-11 11:48:20 +0700

    Add new and create actions with view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,5 +1,43 @@
 class PlaysController < ApplicationController
+  before_action :set_play, only: [:show, :edit, :update, :destroy]
   def index
     
+  end
+
+  def show
+  end
+  
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+    if @play.save
+      redirect_to @play
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
+
+  def set_play
+    @play = Play.find(params[:id])
   end
 end
